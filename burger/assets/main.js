e84d059211d5068ea0905b911e082087fbfef43a
krotosuanew <121033570+krotosuanew@users.noreply.github.com>
2022-12-21 12:23:31 +0300

    aadddd
    add file

---
+++ burger/assets/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $(".header__burger").click(function (event) {
+    $(".header__burger,.header__menu").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
+
+  console.log("comment");
+  console.log("comment");
+  console.log("comment");
+  console.log("comment");
+  console.log("comment");
+  console.log("recommend");
+});
